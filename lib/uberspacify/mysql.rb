07bebaab5a70da88b78ef3713ea58912cc4d78f7
DSIW <dsiw@dsiw-it.de>
2012-10-15 20:54:43 +0200

    Fix trailing whitespace

--- lib/uberspacify/mysql.rb
+++ lib/uberspacify/mysql.rb
@@ -9,14 +9,14 @@ Capistrano::Configuration.instance.load do
       my_cnf = capture('cat ~/.my.cnf')
       config = {}
       %w(development production test).each do |env|
-        
+
         config[env] = {
           'adapter' => 'mysql2',
           'encoding' => 'utf8',
           'database' => "#{fetch :user}_rails_#{fetch :application}_#{env}",
           'host' => 'localhost'
         }
-        
+
         my_cnf.match(/^user=(\w+)/)
         config[env]['username'] = $1
 
@@ -25,13 +25,13 @@ Capistrano::Configuration.instance.load do
 
         my_cnf.match(/^port=(\d+)/)
         config[env]['port'] = $1.to_i
-        
+
         run "mysql -e 'CREATE DATABASE IF NOT EXISTS #{config[env]['database']} CHARACTER SET utf8 COLLATE utf8_general_ci;'"
       end
-      
+
       run "mkdir -p #{fetch :shared_path}/config"
       put config.to_yaml, "#{fetch :shared_path}/config/database.yml"
-      
+
     end
   end
 
